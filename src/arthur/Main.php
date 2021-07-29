4c203a763fc5daeb3cb94599929b3b6773352b51
Arthur-iv-studios <82117078+Arthur-iv-studios@users.noreply.github.com>
2021-07-28 20:05:44 -0400

    Added

---
+++ src/arthur/Main.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace arthur;
+
+use pocketmine\block\BlockFactory;
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\plugin\PluginBase;
+use pocketmine\resourcepacks\ZippedResourcePack;
+
+
+class Main extends PluginBase implements Listener{
+    /**
+     * When the plugin enables
+     *
+     * @return void
+     */
+    public function onEnable(){
+        //3D Guns Loader
+		if(!file_exists($this->getDataFolder())){
+			mkdir($this->getDataFolder());
+		}
+        $downRP = false;
+        if(!file_exists($this->getDataFolder() . "Guns.mcpack")) {
+            $downRP = true;
+            file_put_contents($this->getDataFolder() . "Guns.mcpack");
+        }
+        //Replaces Bow With a 3D GUN
+        $pack = new ZippedResourcePack($this->getDataFolder() . "Guns.mcpack");
+        $r = new \ReflectionClass("pocketmine\\resourcepacks\\ResourcePackManager");
+        $resourcePacks = $r->getProperty("resourcePacks");
+        $resourcePacks->setAccessible(true);
+        $rps = $resourcePacks->getValue($this->getServer()->getResourceManager());
+        $rps[] = $pack;
+        $resourcePacks->setValue($this->getServer()->getResourceManager(), $rps);
+        $resourceUuids = $r->getProperty("uuidList");
+        $resourceUuids->setAccessible(true);
+        $uuids = $resourceUuids->getValue($this->getServer()->getResourceManager());
+        $uuids[$pack->getPackId()] = $pack;
+        $resourceUuids->setValue($this->getServer()->getResourceManager(), $uuids);
+        $forceResources = $r->getProperty("serverForceResources");
+        $forceResources->setAccessible(true);
+        $forceResources->setValue($this->getServer()->getResourceManager(), true);
+        }
+    }
+}
